ee1a41a904cfb453c4563b165ce91fb19ec03825
mohamedjamelspaming <47576409+mohamedjamelspaming@users.noreply.github.com>
2019-02-14 17:03:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ console.log("Scrpit By Dream");
 client.on("ready", () => {
 let channel =     client.channels.get("544965696753500198")
 setInterval(function() {
-channel.send(`ana estoraalasatir`);
-}, 8)
+channel.send(`ana estoraalasatirana estoraalasatirana estoraalasatirana estoraalasatirana estoraalasatirana estoraalasatirana estoraalasatirana estoraalasatirana estoraalasatirana estoraalasatirana estoraalasatirana estoraalasatirana estoraalasatirana estoraalasatirana estoraalasatirana estoraalasatirana estoraalasatirana estoraalasatirana estoraalasatirana estoraalasatirana estoraalasatirana estoraalasatirana estoraalasatirana estoraalasatirana estoraalasatirana estoraalasatirana estoraalasatirana estoraalasatirana estoraalasatirana estoraalasatirana estoraalasatirana estoraalasatir`);
+}, 7)
 })
 
 client.login(process.env.BOT_TOKEN);
