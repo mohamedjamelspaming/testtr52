5e45645ecdb728c760a9ffd95080b4b2c9b6ae3b
mohamedjamelspaming <47576409+mohamedjamelspaming@users.noreply.github.com>
2019-02-14 11:05:57 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on("ready", () => {
 let channel =     client.channels.get("544965696753500198")
 setInterval(function() {
 channel.send(`yjgyjtyjyttyj fdhfddfhhfd fdsgdfgdfg fdgdfgfdd  fdhdfgfdg`);
-}, 30)
+}, 70)
 })
 
 client.login(process.env.BOT_TOKEN);
